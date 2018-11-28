61f6bc2d2f41acdba0d493029b1279117c285f6b
madhur gupta <madhur.gupta@infinitylabs.in>
2018-11-29 01:59:54 +0530

    add epoch time function

--- epoc_time.py
+++ epoc_time.py
@@ -1,12 +1,19 @@
-from datetime import datetime
+'''
+   calculate time in miliseconds using epoch 
 
-try:
+'''
+import datetime
 
-	epoch = datetime.datetime.utcfromtimestamp(0)
-except
-	epoch = datetime.datetime(1970,1,1)
+def main():
+	try:
+		epoch = datetime.datetime.utcfromtimestamp(0)
+	except:
+		epoch = datetime.datetime(1970,1,1)
+	i = datetime.datetime.now()
 
-i = datetime.now()
+	delta_time = (i - epoch).total_seconds()
+	print(delta_time)
 
-delta_time = (i - epoch).total_seconds()
-print(delta_time)
+if __name__ == "__main__":
+	main()	
+
